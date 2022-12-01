1d6003476ad218fddc2050a62be97ff8aac0f4f4
PeetV <peetjvermeulen@gmail.com>
2022-12-01 20:20:04 +1100

    Day 1

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿Day1.SolutionPart2();
+
+public static class Day1
+{
+
+    public static void SolutionPart1()
+    {
+        List<int> totals = GetTotals();
+        Console.WriteLine($"Highest total: {totals.Max()}");
+    }
+
+    public static void SolutionPart2()
+    {
+        List<int> totals = GetTotals();
+        totals.Sort();
+        totals.Reverse();
+        int result = totals[0] + totals[1] + totals[2];
+        Console.WriteLine($"Sum of highest 3: {result}");
+    }
+
+    public static List<int> GetTotals()
+    {
+        int rowval, index = 0;
+        string[] data = File.ReadLines("Data/day1.txt").ToArray();
+        List<int> totals = new() { 0 };
+        foreach (string row in data)
+        {
+            if (row == "")
+            {
+                index += 1;
+                totals.Add(0);
+                continue;
+            }
+            if (!int.TryParse(row, out rowval)) rowval = 0;
+            totals[index] += rowval;
+        }
+        return totals;
+    }
+
+}
